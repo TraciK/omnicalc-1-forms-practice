d386a1da12dfd1be38cbe524924ccba62e95e02b
TraciK <70920574+TraciK@users.noreply.github.com>
2025-08-08 16:26:42 +0000

    edits

--- app/controllers/omni_controller.rb
+++ app/controllers/omni_controller.rb
@@ -3,6 +3,7 @@ class OmniController < ApplicationController
     render({ :template => "omni_templates/homepage" })
   end
   def square_results
+    @user_number = params[:user_number].to_f
     render({ :template => "omni_templates/square_results" })
   end
 end
