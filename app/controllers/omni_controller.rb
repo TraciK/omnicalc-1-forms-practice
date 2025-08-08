af54a94fe6acc1f33959a6740de5852e2b6fe5bf
TraciK <70920574+TraciK@users.noreply.github.com>
2025-08-08 02:33:30 +0000

    Edits

--- app/controllers/omni_controller.rb
+++ app/controllers/omni_controller.rb
@@ -2,4 +2,7 @@ class OmniController < ApplicationController
  def render_homepage
     render({ :template => "omni_templates/homepage" })
   end
+  def square_results
+    render({ :template => "omni_templates/square_results" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "omni", :action => "render_homepage" })
 
-  get("/square_root", { :controller => "omni", :action => "square_root" })
+  get("/square", { :controller => "omni", :action => "square" })
+
+    get("/square_results", { :controller => "omni", :action => "square_results" })
 end
