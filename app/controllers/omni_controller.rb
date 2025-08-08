6e616ffb8a7c84fc6ec6e68072fd3a74f19b60af
TraciK <70920574+TraciK@users.noreply.github.com>
2025-08-08 18:38:47 +0000

    Fixed payments

--- app/controllers/omni_controller.rb
+++ app/controllers/omni_controller.rb
@@ -15,4 +15,23 @@ class OmniController < ApplicationController
     def payment
     render({ :template => "omni_templates/payment" })
   end
+
+    def payment_results
+    apr_string           = params.fetch("user_apr")
+    years_string         = params.fetch("user_num_years")
+    present_value_string = params.fetch("user_principal")
+
+    apr   = apr_string.to_f
+    years = years_string.to_i
+    pv    = present_value_string.to_f
+
+    r = apr / 100 / 12
+    n = years * 12
+
+    @numerator   = r * pv
+    @denominator = 1 - (1 + r) ** -n
+    @payment     = @numerator / @denominator
+
+    render({ :template => "omni_templates/payment_results" })
+  end
 end
